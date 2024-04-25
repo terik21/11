05dc169baa6c1c789001d785f33139f9530b8d66
terik21 <151690487+terik21@users.noreply.github.com>
2024-04-25 17:06:15 +0300

    Add files via upload

---
+++ 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace pc {
+    class Programm { 
+        static void Main()
+        {
+            int countp = int.Parse(Console.ReadLine());
+            comp(countp);
+        }
+        public static void comp(int i)
+        {
+            if (i % 10 == 1 && (i % 100 > 20 || i % 100 < 2))
+            {
+                Console.WriteLine($"{i} компьютер");
+            }
+            else if (i % 10 < 5 && (i % 100 > 20 || i % 100 < 5)) 
+            { 
+                Console.WriteLine($"{i} компьютера"); 
+            }
+            else
+            {
+                Console.WriteLine($"{i} компьютеров");
+            }
+        }
+    }
+}
